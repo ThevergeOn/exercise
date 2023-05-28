df3fecf5526160342daf5b402a0e6a1c38af4b1f
zahra <barqamadizahra@gmail.com>
2023-05-29 01:23:03 +0430

    implement design requirement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-hhhhhhhhhhhhhhhhhhhhhi
+      <div className="side-div-container">
+        <div className="side-div"></div>
+        <div className="side-div"></div>
+      </div>
+
+      <div className="center-div">
+        <div class="center-circle ">
+          <div class="center-circle  circle-1"></div>
+          <div class="center-circle  circle-2"></div>
+          <div class="center-circle  circle-3"></div>
+          <div class="center-circle  circle-4"></div>
+        </div>
+        <div class="circle top-left"></div>
+        <div class="circle top-right"></div>
+        <div class="circle bottom-left"></div>
+        <div class="circle bottom-right"></div>
+      </div>
+
+      <div className="side-div-container">
+        <div className="side-div"></div>
+        <div className="side-div"></div>
+      </div>
     </div>
   );
 }
